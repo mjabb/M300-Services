bdd572fd983e78e009b6d49d0da2c3d1e8bfcb96
Marwane Jabbes <marwane.jabbes@edu.tbz.ch>
2022-03-14 10:56:32 +0100

    IP Update

--- lb2/db.sh
+++ lb2/db.sh
@@ -4,8 +4,8 @@
 #
 
 # root Password setzen, damit kein Dialog erscheint und die Installation haengt!
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password S3cr3tp4ssw0rd'
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password S3cr3tp4ssw0rd'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password Passw0rd!'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password Passw0rd!'
 
 # Installation
 sudo apt-get install -y mysql-server
@@ -13,10 +13,10 @@ sudo apt-get install -y mysql-server
 # MySQL Port oeffnen
 sudo sed -i -e"s/^bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/" /etc/mysql/mysql.conf.d/mysqld.cnf
 
-# User fuer Remote Zugriff einrichten - aber nur fuer Host web 192.168.55.101
-mysql -uroot -pS3cr3tp4ssw0rd <<%EOF%
-	CREATE USER 'root'@'192.168.55.101' IDENTIFIED BY 'admin';
-	GRANT ALL PRIVILEGES ON *.* TO 'root'@'192.168.55.101';
+# User fuer Remote Zugriff einrichten - aber nur fuer Host web 192.168.1.99
+mysql -uroot -pPassw0rd! <<%EOF%
+	CREATE USER 'root'@'192.168.2.99' IDENTIFIED BY 'admin';
+	GRANT ALL PRIVILEGES ON *.* TO 'root'@'192.168.2.99';
 	FLUSH PRIVILEGES;
 %EOF%
 
